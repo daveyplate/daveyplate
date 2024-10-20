e531216cf006946c59d56f63fbb423d8a13c71c4
Dave <david@swiftapk.com>
2024-10-19 22:04:48 -0700

    Update user-avatar.jsx

--- src/components/user-avatar.jsx
+++ src/components/user-avatar.jsx
@@ -1,6 +1,17 @@
 import React, { forwardRef } from 'react'
 import { Avatar, cn } from '@nextui-org/react'
 
+/**
+ * A user avatar component that displays a user's avatar image and name.
+ * @type {React.ForwardRefExoticComponent<React.PropsWithoutRef<import("@nextui-org/react").AvatarProps> & {user: Object; locale?: string;} & React.RefAttributes<HTMLElement>>}
+ * @param {Object} props The properties of the component.
+ * @param {Object} props.user The user object.
+ * @param {string} props.user.name The user's name.
+ * @param {string} props.user.username The user's username.
+ * @param {string} props.user.avatar_url The URL of the user's avatar image.
+ * @param {string} props.locale The locale of the user.
+ * @returns {JSX.Element} The user avatar component.
+ */
 const UserAvatar = forwardRef(({ user, ...props }, ref) => {
     let textSize
     switch (props.size) {
@@ -25,7 +36,5 @@ const UserAvatar = forwardRef(({ user, ...props }, ref) => {
         />
     )
 })
-
-UserAvatar.displayName = 'UserAvatar'
 
 export default UserAvatar
