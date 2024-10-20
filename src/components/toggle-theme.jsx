a4fa4858170c82664e2763340400738f1704a771
Dave <david@swiftapk.com>
2024-10-20 16:09:20 -0700

    Update toggle-theme.jsx

--- src/components/toggle-theme.jsx
+++ src/components/toggle-theme.jsx
@@ -21,9 +21,9 @@ const ToggleTheme = forwardRef((props, ref) => {
         >
             <DropdownTrigger>
                 <Button isIconOnly variant="light">
-                    <SunIcon className="size-6 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                    <SunIcon className="size-6 rotate-0 scale-100 dark:-rotate-90 dark:scale-0" />
 
-                    <MoonIcon className="size-5 absolute rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                    <MoonIcon className="size-5 absolute rotate-90 scale-0 dark:rotate-0 dark:scale-100" />
 
                     <span className="sr-only">
                         <AutoTranslate tKey="toggle_theme" namespace="toggle_theme">
