1a7794a4fa33421d9a5d8b2c650d4b2fdc8c497c
Dave <david@swiftapk.com>
2024-10-20 16:18:29 -0700

    Fix transpile issue

--- export.config.js
+++ export.config.js
@@ -1,26 +1,5 @@
-const withTM = require('next-transpile-modules')(['@stampmyvisa/react-flagpack']);
-
 /** @type {import('next').NextConfig} */
-module.exports = withTM({
+module.exports = {
     output: "export",
-    images: { unoptimized: true },
-    webpack: (config, { isServer }) => {
-        config.module.rules.push({
-            test: /\.svg$/,
-            include: /node_modules\/@stampmyvisa\/react-flagpack\/dist\/flags/,
-            use: [
-                {
-                    loader: 'file-loader',
-                    options: {
-                        name: '[path][name].[hash].[ext]', // Update this line
-                        outputPath: 'static/flags/',
-                        publicPath: '/_next/static/flags/',
-                        emitFile: !isServer,
-                    },
-                },
-            ],
-        });
-
-        return config;
-    },
-})
+    images: { unoptimized: true }
+}
--- next.config.js
+++ next.config.js
@@ -1,27 +1,7 @@
-const withTM = require('next-transpile-modules')(['@stampmyvisa/react-flagpack']);
 const { i18n } = require('./next-i18next.config')
 
 /** @type {import('next').NextConfig} */
-module.exports = withTM({
+module.exports = {
   i18n,
-  reactStrictMode: true,
-  webpack: (config, { isServer }) => {
-    config.module.rules.push({
-      test: /\.svg$/,
-      include: /node_modules\/@stampmyvisa\/react-flagpack\/dist\/flags/,
-      use: [
-        {
-          loader: 'file-loader',
-          options: {
-            name: '[path][name].[hash].[ext]', // Update this line
-            outputPath: 'static/flags/',
-            publicPath: '/_next/static/flags/',
-            emitFile: !isServer,
-          },
-        },
-      ],
-    });
-
-    return config;
-  },
-})
+  reactStrictMode: true
+}
