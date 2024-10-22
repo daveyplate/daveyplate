4883c1d6c7eb9abb6799167af6ff531a5c606770
Dave <david@swiftapk.com>
2024-10-21 21:58:22 -0700

    Update logout.js

--- src/pages/logout.js
+++ src/pages/logout.js
@@ -1,18 +1,23 @@
-import { createClient } from "@/utils/supabase/component"
-import { useRouter } from "next/router"
 import { useEffect } from "react"
+import { useRouter } from "next/router"
+import { Capacitor, CapacitorCookies } from '@capacitor/core'
 
+import { useClearCache } from "@daveyplate/supabase-swr-entities"
+
+import { Spinner } from "@nextui-org/react"
+
+import { createClient } from "@/utils/supabase/component"
 import { getStaticPaths as getExportStaticPaths } from "@/utils/get-static"
 import { getTranslationProps } from '@/utils/translation-props'
 import { isExport } from "@/utils/utils"
 
 import { localeHref } from "@/components/locale-link"
-import { Capacitor, CapacitorCookies } from '@capacitor/core'
 import PageTitle from "@/components/page-title"
 
 export default ({ locale }) => {
     const supabase = createClient()
     const router = useRouter()
+    const clearCache = useClearCache()
 
     useEffect(() => {
         if (Capacitor.isNativePlatform()) {
@@ -20,13 +25,20 @@ export default ({ locale }) => {
             CapacitorCookies.clearCookies()
         }
 
-        supabase.auth.signOut().finally(() =>
+        supabase.auth.signOut(
+            { scope: "local" }
+        ).finally(() => {
             router.replace(localeHref("/login", locale))
-        )
+            clearCache()
+        })
     }, [])
 
     return (
-        <PageTitle title="" />
+        <div className="flex-center">
+            <PageTitle title="" />
+
+            <Spinner color="current" />
+        </div>
     )
 }
 
