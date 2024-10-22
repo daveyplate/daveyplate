73babe5499783cdedb586fd545d195f8360e71ba
Dave <david@swiftapk.com>
2024-10-22 00:23:22 -0700

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,8 +7,8 @@ export default function Footer({ locales, locale }) {
     const year = new Date().getFullYear()
 
     return (
-        <footer className="backdrop-blur-xl bg-background/70 pb-safe sticky bottom-0 z-10">
-            <div className="flex justify-center items-center py-2 gap-2 overflow-hidden">
+        <footer className="backdrop-blur-xl bg-background/70 sticky bottom-0 z-10">
+            <div className="flex justify-center items-center py-2 gap-2 overflow-hidden mb-safe">
                 <LocaleDropdown locales={locales} locale={locale} isIconOnly variant="light" />
 
                 <motion.div
