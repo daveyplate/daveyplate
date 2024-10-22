bc2285d43bffc677075a6d176a86b0ca6f01dc0a
Dave <david@swiftapk.com>
2024-10-22 00:36:36 -0700

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -13,6 +13,11 @@ const MyApp = ({ Component, pageProps }) => {
     return (
         <Providers {...pageProps}>
             <Head>
+                <meta
+                    name='viewport'
+                    content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover'
+                />
+
                 <meta name="description" content="Description" />
                 <meta name="keywords" content="Keywords" />
 
