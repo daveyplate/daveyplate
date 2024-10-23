d1e14a242e99eb18c79c63e6def7285665cfa3ca
Davey <david@swiftapk.com>
2024-10-23 03:19:21 -0700

    Update test-loader.js

--- src/pages/api/test-loader.js
+++ src/pages/api/test-loader.js
@@ -1,6 +1,8 @@
 import { promises as fs } from 'fs'
+import path from 'path'
 
 export default async function handler(req, res) {
-    const file = await fs.readFile(process.cwd() + '/entity.schemas.json', 'utf8');
+    const filePath = path.join(process.cwd(), 'entity.schemas.json');
+    const file = await fs.readFile(filePath, 'utf8');
     res.json(JSON.parse(file))
 }
