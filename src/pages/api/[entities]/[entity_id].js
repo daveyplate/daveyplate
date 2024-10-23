f76910ae12745184f2708658d39fa740c28c1ec0
Dave <david@swiftapk.com>
2024-10-23 12:39:30 -0700

    Latest supabase entities

--- src/pages/api/[entities]/[entity_id].js
+++ src/pages/api/[entities]/[entity_id].js
@@ -3,7 +3,6 @@ import Cors from "cors"
 const cors = Cors()
 
 import { createClient } from '@/utils/supabase/api'
-import { createClient as createAdminClient } from '@/utils/supabase/service-role'
 
 import { runMiddleware } from '@/utils/utils'
 
@@ -11,11 +10,9 @@ export default async (req, res) => {
     await runMiddleware(req, res, cors)
 
     const supabase = createClient(req, res)
-    const supabaseAdmin = createAdminClient()
 
     const response = await entityRoute({
         supabase,
-        supabaseAdmin,
         ...req
     })
 
