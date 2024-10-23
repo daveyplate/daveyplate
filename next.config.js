ae41044d3554f706305252dc403ff7f3f07febf3
Davey <david@swiftapk.com>
2024-10-23 03:05:48 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,5 +3,8 @@ const { i18n } = require('./next-i18next.config')
 /** @type {import('next').NextConfig} */
 module.exports = {
   i18n,
-  reactStrictMode: true
+  reactStrictMode: true,
+  experimental: {
+    nftTracing: true
+  }
 }
