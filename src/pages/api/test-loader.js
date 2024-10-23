f8f520726e281c639ce02cd61b73e63ef449f647
Davey <david@swiftapk.com>
2024-10-23 03:26:57 -0700

    Delete test-loader.js

--- src/pages/api/test-loader.js
+++
@@ -1,8 +0,0 @@
-import { promises as fs } from 'fs'
-import path from 'path'
-
-export default async function handler(req, res) {
-    const filePath = path.join(process.cwd(), 'entity.schemas.json');
-    const file = await fs.readFile(filePath, 'utf8');
-    res.json(JSON.parse(file))
-}
