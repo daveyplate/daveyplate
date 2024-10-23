26bee078c16853c7a0b257fa933bdb13bf8f45cf
Dave <david@swiftapk.com>
2024-10-23 13:54:14 -0700

    Updated entity schema

--- src/utils/supabase/server.js
+++ src/utils/supabase/server.js
@@ -5,25 +5,23 @@ export function createClient() {
     const cookieStore = cookies()
 
     return createServerClient(
-        process.env.NEXT_PUBLIC_SUPABASE_URL,
-        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+        process.env.SUPABASE_URL,
+        process.env.SUPABASE_ANON_KEY,
         {
             cookies: {
-                getAll() {
-                    return cookieStore.getAll()
+                async getAll() {
+                    return (await cookieStore).getAll()
                 },
-                setAll(cookiesToSet) {
+                async setAll(cookiesToSet) {
                     try {
+                        const resolvedCookiesStore = await cookieStore
+
                         cookiesToSet.forEach(({ name, value, options }) =>
-                            cookieStore.set(name, value, options)
+                            resolvedCookiesStore.set(name, value, options),
                         )
-                    } catch {
-                        // The `setAll` method was called from a Server Component.
-                        // This can be ignored if you have middleware refreshing
-                        // user sessions.
-                    }
+                    } catch { }
                 },
             },
-        }
+        },
     )
 }
