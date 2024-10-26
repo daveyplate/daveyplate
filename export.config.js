98356198a2e9da47e185d5b17adab803acbbd55d
Davey <david@swiftapk.com>
2024-10-26 02:47:39 -0700

    Update export.config.js

--- export.config.js
+++ export.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
     output: "export",
-    images: { unoptimized: true }
+    images: { unoptimized: true },
+    experimental: {
+        scrollRestoration: true
+    }
 }
