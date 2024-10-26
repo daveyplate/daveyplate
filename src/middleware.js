d2c6ce67d1c646fff00dff6b49f7d05048e4e5fd
Dave <david@swiftapk.com>
2024-10-25 21:28:43 -0700

    Update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -2,14 +2,14 @@ import { NextResponse } from 'next/server'
 import { rateLimit } from '@daveyplate/next-rate-limit'
 
 export function middleware(request) {
-    const nextResponse = NextResponse.next()
+    const response = NextResponse.next()
 
     if (process.env.NODE_ENV == 'production') {
-        const rateLimitResponse = rateLimit({ request, nextResponse })
+        const rateLimitResponse = rateLimit({ request, response })
         if (rateLimitResponse) return rateLimitResponse
     }
 
-    return nextResponse
+    return response
 }
 
 // Apply middleware to all API routes
