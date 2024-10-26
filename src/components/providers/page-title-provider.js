17123b5d85d2f45d8c70a889763cdd219088100c
Dave <david@swiftapk.com>
2024-10-26 14:18:54 -0700

    Update page-title-provider.js

--- src/components/providers/page-title-provider.js
+++ src/components/providers/page-title-provider.js
@@ -1,5 +1,5 @@
 import Head from "next/head"
-import { createContext, useContext, useEffect, useRef, useState } from 'react'
+import { createContext, useContext, useEffect, useState } from 'react'
 import { useRouter } from "next/router"
 
 const PageTitleContext = createContext()
