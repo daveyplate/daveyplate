3f46c5b01cc00d9e4bba7d7dac9b5bf18caf2c44
Dave <david@swiftapk.com>
2024-11-07 20:07:28 -0800

    @daveyplate/next-cors-middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,46 +1,11 @@
-import { NextResponse } from 'next/server'
 import { rateLimit } from '@daveyplate/next-rate-limit'
+import { nextCors } from '@daveyplate/next-cors-middleware'
 
 const allowedOrigins = ['http://localhost:3000']
 
-const corsOptions = {
-    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-}
-
-function nextCors({ request, allowedOrigins, corsOptions }) {
-    // Check the origin from the request
-    const origin = request.headers.get('origin') ?? ''
-    const isAllowedOrigin = allowedOrigins.includes(origin)
-
-    // Handle preflighted requests
-    const isPreflight = request.method === 'OPTIONS'
-
-    if (isPreflight) {
-        const preflightHeaders = {
-            ...(isAllowedOrigin && { 'Access-Control-Allow-Origin': origin }),
-            ...corsOptions,
-        }
-        return NextResponse.json({}, { headers: preflightHeaders })
-    }
-
-    // Handle simple requests
-    const response = NextResponse.next()
-
-    if (isAllowedOrigin) {
-        response.headers.set('Access-Control-Allow-Origin', origin)
-    }
-
-    Object.entries(corsOptions).forEach(([key, value]) => {
-        response.headers.set(key, value)
-    })
-
-    return response
-}
-
 export function middleware(request) {
     // CORS
-    const response = nextCors({ request, allowedOrigins, corsOptions })
+    const response = nextCors({ request, allowedOrigins })
 
     // Rate Limiting
     if (process.env.NODE_ENV === 'production') {
