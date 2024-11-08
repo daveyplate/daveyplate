f205ba3631ec1efb813e155eb105333cb16f49ff
Dave <david@swiftapk.com>
2024-11-07 20:36:58 -0800

    Update schema, packages

--- schema.sql
+++ schema.sql
@@ -28,6 +28,51 @@ create policy none_shall_pass on public.profiles
     for select
     using (false);
 
+-- inserts a row into public.profiles
+create function public.handle_new_user()
+returns trigger
+language plpgsql
+security definer set search_path = ''
+as $$
+begin
+  insert into public.profiles (id, avatar_url, full_name)
+  values (
+    new.id,
+    new.raw_user_meta_data->>'avatar_url',
+    COALESCE(NULLIF(new.raw_user_meta_data->>'full_name', ''), split_part(new.email, '@', 1))
+  );
+  return new;
+end;
+$$;
+
+-- trigger the function every time a user is created
+create trigger on_auth_user_created
+  after insert on auth.users
+  for each row execute procedure public.handle_new_user();
+
+-- Messages table
+create table
+  public.messages (
+    id uuid not null default gen_random_uuid (),
+    content text null,
+    user_id uuid not null,
+    created_at timestamp with time zone not null default now(),
+    updated_at timestamp with time zone not null default now(),
+    constraint messages_pkey primary key (id),
+    constraint messages_user_id_fkey foreign key (user_id) references profiles (id) on delete cascade
+  );
+
+create trigger handle_updated_at before
+  update on public.messages for each row
+  execute function moddatetime ('updated_at');
+
+alter table public.messages enable row level security;
+
+create policy none_shall_pass on public.messages
+    for select
+    using (false);
+
+-- Peers table
 create table
   public.peers (
     id uuid not null default gen_random_uuid (),
@@ -53,33 +98,12 @@ create policy none_shall_pass on public.peers
     for select
     using (false);
 
+-- Cron job to clean out peers every 5 minutes
 SELECT cron.schedule(
- 'delete_old_peers',   -- name of the job
- '*/5 * * * *',       -- cron schedule: every 5 minutes
+ 'delete_old_peers',
+ '*/5 * * * *',
  $$
  DELETE FROM peers
  WHERE updated_at < (NOW() - INTERVAL '5 minutes');
- $$ -- here document syntax for the SQL statement
+ $$
 );
-
--- inserts a row into public.profiles
-create function public.handle_new_user()
-returns trigger
-language plpgsql
-security definer set search_path = ''
-as $$
-begin
-  insert into public.profiles (id, avatar_url, full_name)
-  values (
-    new.id,
-    new.raw_user_meta_data->>'avatar_url',
-    COALESCE(NULLIF(new.raw_user_meta_data->>'full_name', ''), split_part(new.email, '@', 1))
-  );
-  return new;
-end;
-$$;
-
--- trigger the function every time a user is created
-create trigger on_auth_user_created
-  after insert on auth.users
-  for each row execute procedure public.handle_new_user();
