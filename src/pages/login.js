8ac3836b8f3ca5b77d18a2405c92c43b2ae3d8d3
Dave <david@swiftapk.com>
2024-11-25 22:46:24 -0800

    More login updates

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,176 +1,220 @@
-import { useEffect, useState } from 'react'
-import { useRouter } from 'next/router'
-import { Capacitor } from '@capacitor/core'
-import { useTheme } from 'next-themes'
+import { useEffect, useState } from "react"
+import { Button, Input, Link, Divider, Card, CardBody, cn, CardHeader, CardFooter } from "@nextui-org/react"
+import { Icon } from "@iconify/react"
+import { useRouter } from "next/router"
 
-import { Auth } from '@supabase/auth-ui-react'
-import { ThemeSupa } from '@supabase/auth-ui-shared'
-import { useSessionContext } from '@supabase/auth-helpers-react'
-import { useIsClient } from '@uidotdev/usehooks'
-
-import { useAutoTranslate } from 'next-auto-translate'
-import { useClearCache } from '@daveyplate/supabase-swr-entities/client'
-
-import { Card, CardBody, cn, Input } from "@nextui-org/react"
-
-import { useLocaleRouter } from "@/i18n/routing"
-import { getLocalePaths } from "@/i18n/locale-paths"
-import { getTranslationProps } from '@/i18n/translation-props'
-import { isExport } from "@/utils/utils"
-import { createClient } from '@/utils/supabase/component'
-
-import DefaultFont from "@/styles/fonts"
-
-export default function Login({ view }) {
-    const router = useRouter()
-    const localeRouter = useLocaleRouter()
-    const supabase = createClient()
-    const { autoTranslate } = useAutoTranslate("login")
-    const { resolvedTheme } = useTheme()
-    const { session, isLoading: sessionLoading } = useSessionContext()
-    const clearCache = useClearCache()
-    const [redirectTo, setRedirectTo] = useState(null)
-    const isClient = useIsClient()
-
-    const defaultReturnTo = "/"
-
-    useEffect(() => {
-        if (Capacitor.isNativePlatform()) {
-            setRedirectTo('com.leaked.daveyplate://login-callback/?returnTo=' + (router.query.returnTo || defaultReturnTo))
-        } else {
-            setRedirectTo(window.location.origin + (router.query.returnTo || defaultReturnTo))
-        }
-    }, [])
-
-    useEffect(() => {
-        if (session) {
-            clearCache()
-            localeRouter.replace(router.query.returnTo || defaultReturnTo)
-        }
-    }, [session])
-
-    const variables = {
-        sign_up: {
-            email_label: autoTranslate('sign_up_email_label', 'Email Address'),
-            password_label: autoTranslate('sign_up_password_label', 'Create a Password'),
-            email_input_placeholder: autoTranslate('sign_up_email_input_placeholder', 'Your email address'),
-            password_input_placeholder: autoTranslate('sign_up_password_input_placeholder', 'Your password'),
-            button_label: autoTranslate('sign_up_button_label', 'Sign up'),
-            loading_button_label: autoTranslate('sign_up_loading_button_label', 'Signing up ...'),
-            social_provider_text: autoTranslate('sign_up_social_provider_text', 'Log in with {{provider}}'),
-            link_text: autoTranslate('sign_up_link_text', "Don't have an account? Sign up"),
-            confirmation_text: autoTranslate('sign_up_confirmation_text', 'Check your email for the confirmation link'),
-        },
-        sign_in: {
-            email_label: autoTranslate('sign_in_email_label', 'Email Address'),
-            password_label: autoTranslate('sign_in_password_label', 'Your Password'),
-            email_input_placeholder: autoTranslate('sign_in_email_input_placeholder', 'Your email address'),
-            password_input_placeholder: autoTranslate('sign_in_password_input_placeholder', 'Your password'),
-            button_label: autoTranslate('sign_in_button_label', 'Log in'),
-            loading_button_label: autoTranslate('sign_in_loading_button_label', 'Logging in ...'),
-            social_provider_text: autoTranslate('sign_in_social_provider_text', 'Log in with {{provider}}'),
-            link_text: autoTranslate('sign_in_link_text', 'Already have an account? Log in'),
-        },
-        magic_link: {
-            email_input_label: autoTranslate('magic_link_email_input_label', 'Email Address'),
-            email_input_placeholder: autoTranslate('magic_link_email_input_placeholder', 'Your email address'),
-            button_label: autoTranslate('magic_link_button_label', 'Log in'),
-            loading_button_label: autoTranslate('magic_link_loading_button_label', 'Logging in ...'),
-            link_text: autoTranslate('magic_link_link_text', 'Email me a log in link'),
-            confirmation_text: autoTranslate('magic_link_confirmation_text', 'Check your email for the log in link'),
-        },
-        forgotten_password: {
-            email_label: autoTranslate('forgotten_password_email_label', 'Email Address'),
-            password_label: autoTranslate('forgotten_password_password_label', 'Your Password'),
-            email_input_placeholder: autoTranslate('forgotten_password_email_input_placeholder', 'Your email address'),
-            button_label: autoTranslate('forgotten_password_button_label', 'Send reset password instructions'),
-            loading_button_label: autoTranslate('forgotten_password_loading_button_label', 'Sending reset instructions ...'),
-            link_text: autoTranslate('forgotten_password_link_text', 'Forgot your password?'),
-            confirmation_text: autoTranslate('forgotten_password_confirmation_text', 'Check your email for the password reset link'),
-        },
-        update_password: {
-            password_label: autoTranslate('update_password_password_label', 'New Password'),
-            password_input_placeholder: autoTranslate('update_password_password_input_placeholder', 'Your new password'),
-            button_label: autoTranslate('update_password_button_label', 'Update password'),
-            loading_button_label: autoTranslate('update_password_loading_button_label', 'Updating password ...'),
-            confirmation_text: autoTranslate('update_password_confirmation_text', 'Your password has been updated'),
-        },
-        verify_otp: {
-            email_input_label: autoTranslate('verify_otp_email_input_label', 'Email Address'),
-            email_input_placeholder: autoTranslate('verify_otp_email_input_placeholder', 'Your email address'),
-            phone_input_label: autoTranslate('verify_otp_phone_input_label', 'Phone number'),
-            phone_input_placeholder: autoTranslate('verify_otp_phone_input_placeholder', 'Your phone number'),
-            token_input_label: autoTranslate('verify_otp_token_input_label', 'Token'),
-            token_input_placeholder: autoTranslate('verify_otp_token_input_placeholder', 'Your Otp token'),
-            button_label: autoTranslate('verify_otp_button_label', 'Verify token'),
-            loading_button_label: autoTranslate('verify_otp_loading_button_label', 'Logging in …'),
-        },
+const authProviders = {
+    apple: {
+        name: "Apple",
+        icon: <Icon className="text-default-500" icon="fa:apple" width={18} />,
+    },
+    discord: {
+        name: "Discord",
+        icon: <Icon icon="logos:discord-icon" width={25} />,
+    },
+    facebook: {
+        name: "Facebook",
+        icon: <Icon icon="logos:facebook" width={23} />,
+    },
+    google: {
+        name: "Google",
+        icon: <Icon icon="flat-color-icons:google" width={24} />,
+    },
+    github: {
+        name: "GitHub",
+        icon: <Icon className="text-default-500" icon="fe:github" width={27} />,
+    },
+    email: {
+        name: "Email",
+        icon: <Icon className="text-2xl" icon="solar:letter-bold" />
+    },
+    password: {
+        name: "Password",
+        icon: <Icon className="text-xl" icon="solar:lock-keyhole-bold" />
     }
+}
 
-    return isClient && (
-        <div className={cn((!session && !sessionLoading) ? "opacity-1" : "opacity-0",
-            "flex-center transition-all max-w-md"
-        )}>
-            <Card fullWidth>
-                <CardBody className="px-4 pb-0">
-                    <Auth
-                        socialLayout='horizontal'
-                        showLinks={true}
-                        view={view || 'sign_in'}
-                        supabaseClient={supabase}
-                        providers={['discord', 'google', 'facebook', 'apple']}
-                        redirectTo={redirectTo}
-                        magicLink={true}
-                        theme={resolvedTheme}
-                        appearance={{
-                            theme: ThemeSupa,
-                            variables: {
-                                default: {
-                                    colors: {
-                                        brand: `hsl(var(--nextui-primary))`,
-                                        brandAccent: `hsl(var(--nextui-primary))`,
-                                    }
-                                }
-                            },
-                            style: {
-                                label: { fontFamily: DefaultFont.style.fontFamily },
-                                button: { fontFamily: DefaultFont.style.fontFamily },
-                                input: { fontFamily: DefaultFont.style.fontFamily },
-                                divider: { fontFamily: DefaultFont.style.fontFamily },
-                                anchor: { fontFamily: DefaultFont.style.fontFamily },
-                            },
-                            className: {
-                                label: '!text-foreground',
-                                button: '!rounded-xl hover:!opacity-90',
-                                input: '!rounded-xl !border-2 focus:!border-foreground',
-                                anchor: '!text-default-500 hover:!text-default-600',
-                            }
-                        }}
-                        localization={{ variables }}
+const viewTitles = {
+    login: "Log In",
+    signup: "Sign Up",
+    "forgot-password": "Forgot Password",
+    "magic-link": "Email"
+}
 
+const viewActions = {
+    login: "Log In",
+    signup: "Sign Up",
+    "forgot-password": "Reset Password",
+    "magic-link": "Continue with Email"
+}
+
+export default function LoginPage({ magicLink = true, nextRouter = true, initialView = "login" }) {
+    const router = useRouter()
+    const [view, setView] = useState(nextRouter ? router.pathname.split("/")[1] : initialView)
+    const providers = ["google", "facebook", "apple", "github"]
+
+    useEffect(() => {
+        if (nextRouter && view != router.pathname.split("/")[1]) {
+            setView(router.pathname.split("/")[1])
+        }
+    }, [router.pathname])
+
+    useEffect(() => {
+        if (nextRouter && view != router.pathname.split("/")[1]) {
+            router.push(`/${view}`)
+        }
+    }, [view])
+
+    return (
+        <div className="flex flex-col grow items-center justify-center p-4 gap-4">
+            <Card fullWidth className="max-w-sm p-2">
+                <CardHeader className="px-4">
+                    <p className="text-xl font-medium">
+                        {viewTitles[view]}
+                    </p>
+                </CardHeader>
+
+                <CardBody className="gap-4">
+                    <form
+                        className="relative flex flex-col gap-3"
+                        onSubmit={(e) => e.preventDefault()}
                     >
                         <Input
                             label="Email Address"
                             name="email"
-                            placeholder="Enter your email"
                             type="email"
-                            labelPlacement="outside"
+                            variant="bordered"
                         />
-                    </Auth>
+
+                        <Input
+                            className={cn(
+                                ["login", "signup"].includes(view) ? "opacity-1" : "opacity-0 -mt-3 !h-0 overflow-hidden",
+                                "transition-all"
+                            )}
+                            label="Password"
+                            name="password"
+                            type="password"
+                            variant="bordered"
+                        />
+
+                        <Button color="primary" type="submit">
+                            {viewActions[view]}
+                        </Button>
+                    </form>
+
+                    <Link
+                        className={cn(
+                            view == "login" ? "opacity-1" : "opacity-0 -mt-4 !h-0 overflow-hidden",
+                            "transition-all self-center cursor-pointer"
+                        )}
+                        size="sm"
+                        onPress={() => setView("forgot-password")}
+                    >
+                        Forgot password?
+                    </Link>
+
+                    <div className="flex items-center gap-4 py-2">
+                        <Divider className="flex-1" />
+
+                        <p className="shrink-0 text-tiny text-default-500">
+                            OR
+                        </p>
+
+                        <Divider className="flex-1" />
+                    </div>
+
+                    <div className="flex flex-col gap-2">
+                        {magicLink && view != "magic-link" && (
+                            <Button
+                                startContent={
+                                    authProviders.email.icon
+                                }
+                                variant="flat"
+                                onPress={() => setView("magic-link")}
+                            >
+                                Continue with Email
+                            </Button>
+                        )}
+
+                        {view == "magic-link" && (
+                            <Button
+                                startContent={
+                                    authProviders.password.icon
+                                }
+                                variant="flat"
+                                onPress={() => setView("login")}
+                            >
+                                Continue with Password
+                            </Button>
+                        )}
+
+                        {providers?.length < 3 && (
+                            <div className="flex flex-col gap-2">
+                                {providers?.map((provider) => (
+                                    <Button
+                                        key={provider}
+                                        startContent={authProviders[provider].icon}
+                                        variant="flat"
+                                    >
+                                        Continue with {authProviders[provider].name}
+                                    </Button>
+                                ))}
+                            </div>
+                        )}
+
+
+                        {providers?.length > 2 && (
+                            <div className="flex gap-2">
+                                {providers?.map((provider) => (
+                                    <Button
+                                        key={provider}
+                                        variant="flat"
+                                        className="min-w-0"
+                                        fullWidth
+                                    >
+                                        {authProviders[provider].icon}
+                                    </Button>
+                                ))}
+                            </div>
+                        )}
+                    </div>
                 </CardBody>
+
+                <CardFooter className="flex-col">
+                    <p className={cn(
+                        ["login", "magic-link"].includes(view) ? "opacity-1" : "opacity-0 translate-y-3 h-0 overflow-hidden",
+                        "text-center text-small transition-all"
+                    )}>
+                        Need to create an account?&nbsp;
+                        <Link
+                            size="sm"
+                            onPress={() => setView("signup")}
+                            className="cursor-pointer"
+                        >
+                            Sign Up
+                        </Link>
+                    </p>
+
+                    <p className={cn(
+                        ["signup", "forgot-password"].includes(view) ? "opacity-1" : "opacity-0 translate-y-3 h-0 overflow-hidden",
+                        "text-center text-small transition-all"
+                    )}>
+                        Already have an account?&nbsp;
+
+                        <Link
+                            size="sm"
+                            onPress={() => setView("login")}
+                            className="cursor-pointer"
+                        >
+                            Log In
+                        </Link>
+                    </p>
+                </CardFooter>
             </Card>
 
             <p className="text-center text-small text-default-400">
-                By continuing, you agree to our <a href="/terms" className="underline">Terms</a> and <a href="/privacy" className="underline">Privacy</a>.
+                By continuing, you agree to our <a href="/terms" className="underline">Terms</a> & <a href="/privacy" className="underline">Privacy</a>.
             </p>
         </div>
     )
 }
-
-export async function getStaticProps({ locale, params }) {
-    const translationProps = await getTranslationProps({ locale, params })
-
-    return { props: { ...translationProps } }
-}
-
-export const getStaticPaths = isExport() ? getLocalePaths : undefined
