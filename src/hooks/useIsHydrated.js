9b0c0ceb32f15df86678f9c2ca5a62f9b3173725
Dave <david@swiftapk.com>
2024-11-30 16:29:00 -0800

    Update useIsHydrated.js

--- src/hooks/useIsHydrated.js
+++ src/hooks/useIsHydrated.js
@@ -4,6 +4,11 @@ function subscribe() {
     return () => { }
 }
 
+/**
+ * Custom hook that uses the `useSyncExternalStore` hook to determine if the component is hydrated.
+ *
+ * @returns {boolean} - Returns `true` if the component is hydrated, otherwise `false`.
+ */
 export function useIsHydrated() {
     return useSyncExternalStore(
         subscribe,
