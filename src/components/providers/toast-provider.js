71b8ae8e8a651fa88ca78fabfa2aa470fa317497
daveycodez <david@swiftapk.com>
2024-12-07 12:13:24 -0800

    Update toast-provider.js

--- src/components/providers/toast-provider.js
+++ src/components/providers/toast-provider.js
@@ -1,12 +1,5 @@
-import { useTheme } from "next-themes"
-
-import {
-    CheckCircleIcon,
-    ExclamationCircleIcon,
-    ExclamationTriangleIcon,
-    InformationCircleIcon
-} from "@heroicons/react/24/outline"
 import { Spinner } from "@nextui-org/react"
+import { useTheme } from "next-themes"
 import { Toaster } from "sonner"
 
 export default function ToastProvider() {
@@ -17,10 +10,6 @@ export default function ToastProvider() {
             theme={theme}
             richColors={true}
             icons={{
-                success: <CheckCircleIcon className="size-5 mb-[1px]" />,
-                info: <InformationCircleIcon className="size-5 mb-[1px]" />,
-                warning: <ExclamationTriangleIcon className="size-5 mb-[1px]" />,
-                error: <ExclamationCircleIcon className="size-5 mb-[1px]" />,
                 loading: <Spinner size="sm" color="current" className="me-1 mt-1" />,
             }}
             toastOptions={{
