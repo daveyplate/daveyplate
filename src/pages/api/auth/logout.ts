ee49686a9dc0f302d97423f6db41ca1ade004357
daveycodez <david@swiftapk.com>
2024-12-09 19:14:40 -0800

    /api/auth/logout TypeScript

--- src/pages/api/auth/logout.ts
+++ src/pages/api/auth/logout.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@/utils/supabase/api'
+import { NextApiRequest, NextApiResponse } from 'next'
 
-export default async function handler(req, res) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method !== 'GET') {
         res.status(405).appendHeader('Allow', 'GET').end()
         return
