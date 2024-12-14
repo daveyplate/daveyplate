15f9e082e652ece8337694da2ba4a64252389390
daveycodez <david@swiftapk.com>
2024-12-14 01:02:14 -0800

    More cleanup

--- src/pages/api/[...supabase].ts
+++ src/pages/api/[...supabase].ts
@@ -9,7 +9,7 @@ export const config = {
     }
 }
 
-export default async (req: NextRequest): Promise<Response> => {
+export default async (req: NextRequest) => {
     // Get the Access Token from the Authorization header or the Session
     const supabase = createClient(req)
     const { data: { session } } = await supabase.auth.getSession()
@@ -44,57 +44,6 @@ export default async (req: NextRequest): Promise<Response> => {
             authorization: `Bearer ${newToken}`,
             prefer: 'return=representation',
         },
-        body: req.method !== "GET" && req.method !== "HEAD" ? req.body : null,
+        body: req.body
     })
 }
-
-
-/*
-import { NextApiRequest, NextApiResponse } from 'next'
-import { createProxyMiddleware } from 'http-proxy-middleware'
-import jwt, { JwtPayload } from 'jsonwebtoken'
-
-import { createClient } from '@/utils/supabase/api'
-
-export const config = {
-    runtime: 'edge',
-    api: {
-        externalResolver: true,
-        bodyParser: false
-    }
-}
-
-const apiProxy = createProxyMiddleware({
-    target: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    changeOrigin: true,
-    pathRewrite: { '^/api': '' }
-})
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const supabase = createClient(req, res)
-    const { data: { session } } = await supabase.auth.getSession()
-
-    const accessToken = req.headers.authorization?.replace('Bearer ', '') || session?.access_token
-
-    // Append is_server = true to JWT for RLS
-    const decodedJwt = accessToken ? {
-        ...jwt.verify(accessToken, process.env.SUPABASE_JWT_SECRET!) as JwtPayload,
-        is_server: true
-    } : { is_server: true }
-
-    const newToken = jwt.sign(decodedJwt, process.env.SUPABASE_JWT_SECRET!)
-    req.headers.authorization = `Bearer ${newToken}`
-
-    if (!req.headers.apikey) {
-        req.headers.apikey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-    }
-
-    return new Promise((resolve, reject) => {
-        apiProxy(req, res, (result) => {
-            if (result instanceof Error) return reject(result)
-
-            return resolve(result)
-        })
-    })
-}
-    */
--- src/utils/supabase/api.ts
+++ src/utils/supabase/api.ts
@@ -2,18 +2,6 @@ import { createServerClient, serializeCookieHeader } from '@supabase/ssr'
 import { type NextApiRequest, type NextApiResponse } from 'next'
 
 export function createClient(req: NextApiRequest, res: NextApiResponse) {
-    /*
-    if (req.headers.authorization) {
-        const supabase = createSupabaseClient(
-            process.env.NEXT_PUBLIC_SUPABASE_URL!,
-            process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-            { global: { headers: { Authorization: req.headers.authorization } } }
-        )
-
-        return supabase
-    }
-    */
-
     const supabase = createServerClient(
         process.env.NEXT_PUBLIC_SUPABASE_URL!,
         process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
