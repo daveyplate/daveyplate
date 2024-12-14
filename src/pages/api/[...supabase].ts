5854fd4077bfb5e69a481ed3551b95eb76d432e3
daveycodez <david@swiftapk.com>
2024-12-14 00:47:43 -0800

    Update [...supabase].ts

--- src/pages/api/[...supabase].ts
+++ src/pages/api/[...supabase].ts
@@ -10,7 +10,7 @@ export const config = {
 }
 
 export default async (req: NextRequest): Promise<Response> => {
-    // Prepare the Request context
+    // Begin Request Scope for createClient's cookies()
     NextResponse.next()
 
     // Get the Access Token from the Authorization header or the Session
@@ -50,54 +50,3 @@ export default async (req: NextRequest): Promise<Response> => {
         body: req.method !== "GET" && req.method !== "HEAD" ? req.body : null,
     })
 }
-
-
-/*
-import { NextApiRequest, NextApiResponse } from 'next'
-import { createProxyMiddleware } from 'http-proxy-middleware'
-import jwt, { JwtPayload } from 'jsonwebtoken'
-
-import { createClient } from '@/utils/supabase/api'
-
-export const config = {
-    runtime: 'edge',
-    api: {
-        externalResolver: true,
-        bodyParser: false
-    }
-}
-
-const apiProxy = createProxyMiddleware({
-    target: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    changeOrigin: true,
-    pathRewrite: { '^/api': '' }
-})
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const supabase = createClient(req, res)
-    const { data: { session } } = await supabase.auth.getSession()
-
-    const accessToken = req.headers.authorization?.replace('Bearer ', '') || session?.access_token
-
-    // Append is_server = true to JWT for RLS
-    const decodedJwt = accessToken ? {
-        ...jwt.verify(accessToken, process.env.SUPABASE_JWT_SECRET!) as JwtPayload,
-        is_server: true
-    } : { is_server: true }
-
-    const newToken = jwt.sign(decodedJwt, process.env.SUPABASE_JWT_SECRET!)
-    req.headers.authorization = `Bearer ${newToken}`
-
-    if (!req.headers.apikey) {
-        req.headers.apikey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-    }
-
-    return new Promise((resolve, reject) => {
-        apiProxy(req, res, (result) => {
-            if (result instanceof Error) return reject(result)
-
-            return resolve(result)
-        })
-    })
-}
-    */
